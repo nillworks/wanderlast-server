c5ed5ae05c6be3be76904d10431625a22cea1117
Shipon Roy <shiponworks@gmail.com>
2026-05-15 21:32:20 +0600

    update: verifytoken

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ const app = express();
 const port = process.env.PORT || 8000;
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { createRemoteJWKSet, jwtVerify } = require('jose-cjs');
 
 app.use(express.json());
 app.use(cors());
+
+const jwks = createRemoteJWKSet(
+  new URL(`${process.env.CLIENT_URL}/api/auth/jwks`),
+);
 
 const client = new MongoClient(process.env.DB_URI, {
   serverApi: {
@@ -17,10 +22,31 @@ const client = new MongoClient(process.env.DB_URI, {
   },
 });
 
+// auth
+const verifyToken = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader.split(' ')[1];
+
+  if (!authHeader) {
+    return res.status(401).json({ message: 'unauthorized' });
+  }
+  if (!token) {
+    return res.status(401).json({ message: 'unauthorized' });
+  }
+
+  // verify token
+  try {
+    const { payload } = await jwtVerify(token, jwks);
+    next();
+  } catch (error) {
+    return res.status(403).json({ massage: 'forbidden' });
+  }
+};
+
 async function run() {
   try {
-    await client.connect();
-    await client.db('admin').command({ ping: 1 });
+    // await client.connect();
+    // await client.db('admin').command({ ping: 1 });
 
     const database = client.db('Wanderlust');
     const DestinationsDataCollection = database.collection('data');
@@ -39,7 +65,7 @@ async function run() {
     });
 
     // Data Post
-    app.post('/featured', async (req, res) => {
+    app.post('/featured', verifyToken, async (req, res) => {
       const newFeaturedData = req.body;
       const result =
         await DestinationsDataCollection.insertOne(newFeaturedData);
@@ -47,30 +73,15 @@ async function run() {
     });
 
     // Details Data
-    app.get(
-      '/featured/:id',
-      (req, res, next) => {
-        const header = req.headers.authorization;
+    app.get('/featured/:id', verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id),
+      };
 
-        if (header === 'logged in') {
-          console.log(header);
-          next();
-        } else {
-          res.status(401).json({
-            message: 'Unauthorized Access',
-          });
-        }
-      },
-      async (req, res) => {
-        const id = req.params.id;
-        const query = {
-          _id: new ObjectId(id),
-        };
-
-        const detailsSend = await DestinationsDataCollection.findOne(query);
-        res.send(detailsSend);
-      },
-    );
+      const detailsSend = await DestinationsDataCollection.findOne(query);
+      res.send(detailsSend);
+    });
 
     // Patch
     app.patch('/featured/:id', async (req, res) => {
@@ -110,7 +121,7 @@ async function run() {
     });
 
     // Booking Information Data DB
-    app.get('/booking', async (req, res) => {
+    app.get('/booking', verifyToken, async (req, res) => {
       const cursor = bookingCollection.find();
       const result = await cursor.toArray();
       res.send({
@@ -120,7 +131,7 @@ async function run() {
       });
     });
 
-    app.post('/booking', async (req, res) => {
+    app.post('/booking', verifyToken, async (req, res) => {
       const newBooking = req.body;
       const result = await bookingCollection.insertOne(newBooking);
       res.send(result);
